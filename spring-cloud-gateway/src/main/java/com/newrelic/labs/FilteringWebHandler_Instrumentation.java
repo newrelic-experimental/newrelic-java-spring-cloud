7259fb84ec605c1e8031b067feabfe40b2b255d7
Jadson de Oliveira Rosa <jrosa@uolinc.com>
2021-01-21 18:06:46 -0300

    Improve transactions naming

--- spring-cloud-gateway/src/main/java/com/newrelic/labs/FilteringWebHandler_Instrumentation.java
+++ spring-cloud-gateway/src/main/java/com/newrelic/labs/FilteringWebHandler_Instrumentation.java
@@ -8,8 +8,6 @@ import com.newrelic.api.agent.weaver.Weaver;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.regex.Pattern;
 
@@ -19,46 +17,44 @@ public abstract class FilteringWebHandler_Instrumentation {
     @Trace(dispatcher = true)
     public Mono<Void> handle(ServerWebExchange exchange) {
 
-        final String path = exchange.getRequest().getPath().value();
 
-        final String simplifiedPath;
+        try {
+            final Pattern versionPattern = Pattern.compile("[vV][0-9]{1,}");
+            final Pattern idPattern = Pattern.compile("^(?=[^\\s]*?[0-9])[-{}().:_|0-9]+$");
+            final Pattern codPattern = Pattern.compile("^(?=[^\\s]*?[0-9])(?=[^\\s]*?[a-zA-Z])(?!\\{id\\})[-{}().:_|a-zA-Z0-9]*$");
 
-        final List<String> splitPath = Arrays.asList(path.split("/"));
+            String path = exchange.getRequest().getPath().value();
 
-        if (splitPath.isEmpty()) {
-            simplifiedPath = path;
-        } else {
-            final StringBuilder builder = new StringBuilder();
-            for (String p : splitPath) {
-                builder.append("/").append(normalizePath(p));
+            String simplifiedPath = path;
+
+            final String[] splitPath = path.split("/");
+
+            if (splitPath.length > 0) {
+                simplifiedPath = "";
+                for (String p : splitPath) {
+                    if (versionPattern.matcher(p).matches()) {
+                        simplifiedPath = simplifiedPath.concat("/").concat(p.replaceAll(versionPattern.toString(), "{version}"));
+                    } else if (idPattern.matcher(p).matches()) {
+                        simplifiedPath = simplifiedPath.concat("/").concat(p.replaceAll(idPattern.toString(), "{id}"));
+                    } else if (codPattern.matcher(p).matches()) {
+                        simplifiedPath = simplifiedPath.concat("/").concat(p.replaceAll(codPattern.toString(), "{cod}"));
+                    } else {
+                        simplifiedPath = simplifiedPath.concat("/").concat(p);
+                    }
+                }
             }
-            simplifiedPath = builder.toString();
+
+            NewRelic.setTransactionName("Web", simplifiedPath);
+            NewRelic.getAgent().getLogger().log(Level.FINER,
+                    "spring-cloud-gateway Instrumentation: Setting web transaction name to " + simplifiedPath);
+        } catch (Exception e) {
+            System.out.println("ERROR spring-cloud-gateway Instrumentation: " + e.getMessage());
         }
 
-
-        NewRelic.setTransactionName("Web", simplifiedPath);
-        NewRelic.getAgent().getLogger().log(Level.FINER,
-                "spring-cloud-gateway Instrumentation: Setting web transaction name to " + simplifiedPath);
         Mono<Void> ret = Weaver.callOriginal();
 
         return ret;
     }
 
-    public String normalizePath(String path) {
-
-        final Pattern versionPattern = Pattern.compile("[vV][0-9]{1,}");
-        final Pattern idPattern = Pattern.compile("^(?=[^\\s]*?[0-9])[-{}().:_|0-9]+$");
-        final Pattern codPattern = Pattern.compile("^(?=[^\\s]*?[0-9])(?=[^\\s]*?[a-zA-Z])(?!\\{id\\})[-{}().:_|a-zA-Z0-9]*$");
-
-        if (versionPattern.matcher(path).matches()) {
-            return path.replaceAll(versionPattern.toString(), "{version}");
-        } else if (idPattern.matcher(path).matches()) {
-            return path.replaceAll(idPattern.toString(), "{id}");
-        } else if (codPattern.matcher(path).matches()) {
-            return path.replaceAll(codPattern.toString(), "{cod}");
-        } else {
-            return path;
-        }
-    }
 
 }
